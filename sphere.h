6e4f0e284f5caac3ee4f7221f4a41f24d5ba3cd2
Kiefer Aland <30326191+kieferaland@users.noreply.github.com>
2021-04-18 13:18:03 -0700

    hitable list working

--- sphere.h
+++ sphere.h
@@ -1,9 +1,46 @@
 #ifndef SPHEREH
 #define SPHEREH
 
-int main() 
-{
+#include "hitable.h"
 
+class sphere: public hitable
+{
+	 public: 
+		  sphere(){}
+		  sphere(vec3 cen, float r): centre(cen), radius(r) {};
+		  virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+		  vec3 centre;
+		  float radius;
+};
+
+bool sphere::hit(const ray&r, float t_min, float t_max, hit_record& rec) const
+{
+	 vec3 oc = r.origin() - centre;
+	 float a = dot(r.direction(),r.direction());
+	 float b = dot(oc, r.direction());
+	 float c = dot(oc, oc) - (radius*radius);
+	 float discriminant = (b*b) - (a*c);
+	 if (discriminant > 0)
+	 {
+		  float temp = (-b - sqrt(b*b-a*c))/a;
+		  if(temp < t_max && temp > t_min)
+		  {
+				rec.t = temp;
+				rec.p = r.parameterize(rec.t);
+				rec.normal = (rec.p-centre)/radius;
+				return true;
+		  }
+		  temp = (-b + sqrt(b*b - a*c))/a;
+		  if(temp < t_max && temp > t_min)
+		  {
+				rec.t = temp;
+				rec.p = r.parameterize(rec.t);
+				rec.normal = (rec.p - centre)/radius;
+				return true;
+		  }
+	 }
+	 return false;
 }
 
 #endif
+
