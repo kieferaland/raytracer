03a4decbb57ee7b9c118c2a2fb4c1698f58fb4ab
Kiefer Aland <30326191+kieferaland@users.noreply.github.com>
2021-04-18 14:33:26 -0700

    added metal header

---
+++ metal.h
@@ -0,0 +1,20 @@
+#ifndef METALH
+#define METALH
+
+#include "material.h"
+
+class metal: public material
+{
+	 public:
+		  metal(const vec3& a): albedo(a) {}
+		  virtual bool scatter (const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const 
+		  {
+				vec3 reflected = reflect(normalize(r_in.direction()), rec.normal);
+				scattered = ray(rec.p, reflected);
+				attenuation = albedo;
+				return (dot(scattered.direction(), rec.normal) > 0);
+		  }
+		  vec3 albedo;
+};
+
+#endif
